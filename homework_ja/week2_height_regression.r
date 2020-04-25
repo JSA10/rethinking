fb7a1480e1c8ab49ad6f6666254bb1bd7e71facc
JSA10 <jerome.ahye@gmail.com>
2020-04-25 15:19:56 +0100

    Completed Q1 in week2 homework

--- homework_ja/week2_height_regression.r
+++ homework_ja/week2_height_regression.r
@@ -42,7 +42,7 @@ Individual   weight   expected height   89% interval
 # 3 Evaluate the model (critique)
 
 
-# modelling height  --------------------------------------------------------------
+# modelling height alone --------------------------------------------------------------
 
 
 # goal - model using a normal (gaussian distribution)
@@ -54,7 +54,7 @@ Individual   weight   expected height   89% interval
 #   mu ~ Normal(178, 20)   
 #   sigma ~ Uniform(0, 50) 
 
-
+# priors for height come from lecturer - his height
 
 # adding a predictor variable to the model --------------------------------
 
@@ -63,11 +63,99 @@ Individual   weight   expected height   89% interval
 
 #  linear model 
 #       mu_i = alpha + beta(x_i, - x_bar)
+# using difference between weight and the mean weight
 
 #  priors 
 #       alpha ~ Normal(178, 20)
 #       beta ~ Normal(0, 10)
 #       sigma ~ Uniform(0, 50) 
+
+# explore the data
+hist(d2$height)
+hist(d2$weight)
+plot(d2$height, d2$weight)
+
+d2$weight_diff_mean <- d2$weight - mean(d2$weight, na.rm = TRUE)
+hist(d2$weight_diff_mean)
+plot(d2$height, d2$weight_diff_mean)
+# review chapter as to why - doesn't look to be real difference 
+
+# fairly normal looking data for target variable and looks to be a linear 
+# relationship with weight 
+
+# define the average weight, x-bar
+xbar <- mean(d2$weight)
+
+# define the model 
+mh_w <- quap(
+    alist(
+        height ~ dnorm( mu , sigma ) ,
+        mu <- a + b*( weight - xbar ) ,
+        a ~ dnorm( 178 , 20 ) ,
+        b ~ dlnorm( 0 , 1 ) ,
+        sigma ~ dunif( 0 , 50 )
+    ) ,
+    data=d2 )
+
+# check model paramaters
+precis(mh_w)
+
+## check model covariance
+round( vcov( mh_w ) , 3 )
+
+# visually explore model posterior
+post_mh_w <- extract.samples(mh_w, 1000)
+
+post_mh_w <- extract.samples( mh_w )
+post_mh_w[1:5,]
+
+a_map <- mean(post_mh_w$a)
+b_map <- mean(post_mh_w$b)
+curve( a_map + b_map*(x - xbar) , add=TRUE )
+
+
+
+# use link to look at predicted values
+mu_pred <- link( mh_w )
+str(mu_pred)
+
+# take weights to make predictions from Q table 
+weights_predict <- c(45, 40, 65, 31, 53)
+
+# use link to compute mu for each sample from posterior and for each weight in table
+mu_pred <- link( mh_w , data=data.frame(weight=weights_predict) )
+str(mu_pred)
+
+# summarize the distribution of mu
+mu_mean <- apply( mu_pred , 2 , mean )
+
+# note - used t to transpose the output    
+mu_PI <- t(apply( mu_pred , 2 , PI , prob=0.89 ))
+
+View(mu_mean)
+
+
+# Q ANSWER = create table with predicted values for the 5 weights given
+mu_summary <- bind_cols(data.frame(weights_predict), data.frame(mu_mean), data.frame(mu_PI))
+
+
+
+
+## R code 4.57 - can als oplot summaries 
+
+# plot raw data
+# fading out points to make line and interval more visible
+plot( height ~ weight , data=d2 , col=col.alpha(rangi2,0.5) )
+
+# plot the MAP line, aka the mean mu for each weight
+lines( weights_predict , mu_mean )
+
+# plot a shaded region for 89% PI
+shade( mu_PI , weights_predict )
+
+
+
+# Q2  ---------------------------------------------------------------------
 
 
 
